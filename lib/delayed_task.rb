682842adec54f1cdc78d72e96d8e63aefb88184d
James Phillips <jamesdphillips@gmail.com>
2012-05-01 17:49:37 -0700

    Added missing brackets

--- lib/delayed_task.rb
+++ lib/delayed_task.rb
@@ -1,7 +1,7 @@
 module DelayedTask
   class PerformableTask < Struct.new(:task, :targs)
     def perform
-      args = targs.count > 0 ? "[targs.join(',')]" : ''
+      args = targs.count > 0 ? "[#{targs.join(',')}]" : ''
       cmd  = "rake #{task}#{args}"
       puts "Executing: `#{cmd}`"
       system cmd
