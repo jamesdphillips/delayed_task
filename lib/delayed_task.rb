b58029afc400856cbc7a7113837ff218016ec87c
James Phillips <jamesdphillips@gmail.com>
2012-05-16 16:36:46 -0700

    Now attempts to invoke rake task programmatically before running the task from the command line.

--- lib/delayed_task.rb
+++ lib/delayed_task.rb
@@ -1,10 +1,20 @@
 module DelayedTask
+  require 'rake'
   class PerformableTask < Struct.new(:task, :targs)
     def perform
-      args = targs.count > 0 ? "[#{targs.join(',')}]" : ''
-      cmd  = "rake #{task}#{args}"
-      puts "Executing: `#{cmd}`"
-      system cmd
+      begin
+        task = Rake::Task[task_name]
+        args = {}
+        targs.each_with_index { |arg,i| args[task.arg_names[i]] = arg }
+        puts "Executing task: #{task.name} #{args.inspect}"
+        task.execute args
+      rescue Exception => e
+        args = targs.count > 0 ? "[#{targs.join(',')}]" : ''
+        cmd  = "rake #{task_name}#{args}"
+        puts "Exception: #{e.inspect}"
+        puts "Executing command: `#{cmd}`"
+        system cmd
+      end
     end
   end
 end
